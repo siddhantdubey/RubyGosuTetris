cba2fed2eec41ce0f9f15d14f9bf6f267c8e620f
Siddhant Dubey <sid12.dubey@gmail.com>
2020-12-08 11:46:58 -0500

    fixed more bugs

--- game.rb
+++ game.rb
@@ -1,7 +1,7 @@
 require "gosu"
 require_relative "grid"
 require_relative "square"
-require_relative "shape"
+require_relative "block"
 require_relative "matrix"
 
 class Tetris < Gosu::Window
@@ -9,14 +9,11 @@ class Tetris < Gosu::Window
   COLUMNS = 10 #the number of columns
   SIZE = 40 #the size of the squares
 
-
-  #a lot of the different game scene stuff was taken from our sector_five code in class
-
   def initialize
     super(COLUMNS * SIZE, ROWS * SIZE)#creates the window based on number of rows and columns
     @fall_interval = 500 #used to determine how fast blocks fall, starts slow gets quicker as player score increases
     @grid = Grid.new(ROWS, COLUMNS) #creates the grid
-    @current_shape = Shape.random(@grid) #makes the block to be placed
+    @current_shape = Block.random(@grid) #makes the block to be placed
     
     
     @playingtime = Gosu.milliseconds #used to keep track of how long the game has been going on and how fast the blocks fall
@@ -31,7 +28,7 @@ class Tetris < Gosu::Window
     @speed = 1 #this is used to use the fall() method so that the shapes fall towards the bottom
     @scene = :start #the current scene of the game, either start, game, or end. 
 
-    @title_font = Gosu::Font.new(self, Gosu::default_font_name, 40) #the font for the title
+    @title_font = Gosu::Font.new(self, Gosu::default_font_name, 40)
   end
 
   def draw
@@ -71,10 +68,6 @@ class Tetris < Gosu::Window
   end
 
   def update
-    @game_over = @grid.full_column?
-    if @game_over
-      @scene = :end
-    end
     case @scene
     #can only play the game while the scene is game, otherwise nothing is being moved and the grid is not being updated
     when :game
@@ -89,12 +82,12 @@ class Tetris < Gosu::Window
   end
 
   def step
-     #checks to see if the game is over by going through each column and seeing if the column is full or not
+    @game_over = @grid.full_column? #checks to see if the game is over by going through each column and seeing if the column is full or not
     if !@game_over #as long as the game is not over then continue to pick new blocks, land them, and delete filled rows
       if @current_shape.haslanded?
         @current_shape.land
         @grid.delete_filled_rows
-        @current_shape = Shape.random(@grid)
+        @current_shape = Block.random(@grid)
       else
         @current_shape.fall(@speed)
       end
--- matrix.rb
+++ matrix.rb
@@ -1,34 +1,35 @@
 class Matrix
+    #A matrix is a 3 x 3 box in which squares are arranged to make different blocks. Methods in this class are used to move the shapes/blocks around.
     #A matrix is a 3 x 3 box in which squares are arranged to make different blocks. This is part of the normal tetris game. Methods in this class are used to move the shapes/blocks around.
     #This was the hardest part of the project in our opinion
     #sources: https://stackoverflow.com/questions/38594574/tetris-2d-array-logic (not in ruby which is annoying, but it was understandable enough)
     #no clue how to use the map method beforehand so this helped: https://www.rubyguides.com/2018/10/ruby-map-method/
-    
+
     def initialize(rows = [])
-      @rowsarray = rows
+      @arrayrows = rows
     end
   
     def [](x, y)
       #overloading indexing of arrays here to make it easier for other methods to be written
-      @rowsarray.fetch(y, [])[x]
+      @arrayrows.fetch(y, [])[x]
     end
   
     def []=(x, y, value)
       #also overloading indexing of arrays to format it in a way that would make other methods easier
-      (@rowsarray[y] ||= [])[x] = value
+      (@arrayrows[y] ||= [])[x] = value
       fixorientation
     end
   
     def rotate
-      #rotates the matrix around 
       #helpful stackoverflow answer: https://stackoverflow.com/questions/233850/tetris-piece-rotation-algorithm/8131337
-      self.class.new(@rowsarray.transpose.map(&:reverse))
+      #rotates the matrix around 
+      self.class.new(@arrayrows.transpose.map(&:reverse))
     end
   
     def look(value)
       #looks for a certain value within the matrix b/c the standard methods in ruby wouldn't work as well as I would have liked for this one, also returns the index of the value, this will be used to move squares
-      @rowsarray.each_with_index do |row, y|
-        row.each_with_index do |val, x|
+      @arrayrows.each_with_index do |rows, y|
+        rows.each_with_index do |val, x|
           return [x, y] if val == value
         end
       end
@@ -37,23 +38,23 @@ class Matrix
   
     def trim
       #gets rid of nil value from matrices and removes everything that doesn't provide useful info, like empty columns and rows
-      @rowsarray = @rowsarray.drop_while { |row| row.all?(&:nil?) }
-      @rowsarray = @rowsarray.map { |column| column.drop(empty_columns) }
+      @arrayrows = @arrayrows.drop_while { |rows| rows.all?(&:nil?) }
+      @arrayrows = @arrayrows.map { |column| column.drop(empty_columns) }
     end
   
     def fixorientation
-      #fixes the orientation of the rows in the matrix (a type of fixorientationment i would say)
-      @rowsarray.map! { |row| Array(row) }
-      @rowsarray.map! { |row| row.concat([nil] * (width - row.length)) }
+      #aligns the rows in the matrix together
+      @arrayrows.map! { |rows| Array(rows) }
+      @arrayrows.map! { |rows| rows.concat([nil] * (width - rows.length)) }
     end
   
     def width
       #returns the width of the matrix
-      @rowsarray.map(&:length).max
+      @arrayrows.map(&:length).max
     end
   
     def empty_columns
       #returns columns that are empty, because we don't need to care about them until they're no longer empty
-      @rowsarray.map { |row| row.find_index { |v| v } }.compact.min
+      @arrayrows.map { |rows| rows.find_index { |v| v } }.compact.min
     end
   end
